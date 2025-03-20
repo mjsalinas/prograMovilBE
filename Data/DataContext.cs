0af76b85a1c52f2885c99a2b2cc04fdb77c4486d
Maria Jose Salinas <mariasalinas713@gmail.com>
2025-03-19 19:20:41 -0600

    realizando override de creacion de modelos y su mapping en EntityFramework para poder leer/escribir en la base de datos

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -9,5 +9,17 @@ public class DataContext : DbContext
     
     public DbSet<Usuario> Usuarios { get; set; }
     public DbSet<Inventario> Inventario { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Usuario>().ToTable("usuarios");
+        
+        modelBuilder.Entity<Usuario>().Property(u => u.Nombre).HasColumnName("nombre");
+        modelBuilder.Entity<Usuario>().Property(u => u.Correo).HasColumnName("correo");
+        modelBuilder.Entity<Usuario>().Property(u => u.Contrasena).HasColumnName("contrasena");
+        modelBuilder.Entity<Usuario>().Property(u => u.CreatedAt).HasColumnName("created_at");
+
+    }
     
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,13 +1,23 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProyectoBase.Models;
-
+[Table("usuarios")]
 public class Usuario
 {
     [Key]
+    [Column("id")]
     public Guid Id { get; set; }
+    
+    [Column("nombre")]
     public string Nombre { get; set; }
+    
+    [Column("correo")]
     public string Correo { get; set; }
+    
+    [Column("contrasena")]
     public string Contrasena { get; set; }
+    
+    [Column("created_at")]
     public DateTime CreatedAt { get; set; }
 }
